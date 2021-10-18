e14ecb879f38593f35e1fb0b474cf1e0eb0d0014
DenisCo1100100 <kodek_den2001@mail.ru>
2021-10-18 12:41:10 +0300

    Make level 1

--- DevIncubator_Autopark/Program.cs
+++ DevIncubator_Autopark/Program.cs
@@ -4,9 +4,44 @@ namespace DevIncubator_Autopark
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
-			Console.WriteLine("Hello World!");
+			//1
+			VehicleType[] vehicles = new VehicleType[]
+			{
+				new VehicleType("Bus", 1.2f),
+				new VehicleType("Car"),
+				new VehicleType("Rink", 1.5f),
+				new VehicleType("Tractor", 1.2f)
+			};
+
+			//2, 5, 6
+			float averageCoefficient = 0;
+			for (int i = 0; i < vehicles.Length; i++)
+			{
+				vehicles[i].Display();
+
+				averageCoefficient += vehicles[i].TaxCoefficient;
+			}
+
+			averageCoefficient /= vehicles.Length;
+			Console.WriteLine($"Average tax coefficient = {averageCoefficient:0.00}");
+
+			//3
+			vehicles[^1].TaxCoefficient = 1.3f;
+
+			//4
+			float maxCoefficient = 0;
+			for (int i = 0; i < vehicles.Length; i++)
+			{
+				if (vehicles[i].TaxCoefficient > maxCoefficient)
+					maxCoefficient = vehicles[i].TaxCoefficient;
+			}
+			Console.WriteLine($"Maximum coefficient = {maxCoefficient}");
+
+			//7
+			foreach (var vehicle in vehicles)
+				Console.WriteLine(vehicle);
 		}
 	}
 }
---
+++ DevIncubator_Autopark/VehicleType.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DevIncubator_Autopark
+{
+	class VehicleType
+	{
+		private string _typeName;
+		private float _taxCoefficient;
+
+		public VehicleType()
+		{
+			_typeName = "Vehicle";
+			_taxCoefficient = 1f;
+		}
+
+		public VehicleType(string typeName, float taxCoefficient = 1f)
+		{
+			_typeName = typeName;
+			_taxCoefficient = taxCoefficient;
+		}
+
+		public string TypeName 
+		{
+			get => _typeName;
+			set => _typeName = value;
+		}
+
+		public float TaxCoefficient 
+		{
+			get => _taxCoefficient;
+			set => _taxCoefficient = value;
+		}
+
+		public void Display()
+		{
+			Console.WriteLine($"TypeName = {_typeName}");
+			Console.WriteLine($"TaxCoefficient = {_taxCoefficient}");
+		}
+
+		public override string ToString() => $"{_typeName}, \"{_taxCoefficient}\"";
+	}
+}
