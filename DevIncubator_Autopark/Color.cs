b8b40bbe35bdf46a80cb35276b58a74d63a916a3
DenisCo1100100 <kodek_den2001@mail.ru>
2021-10-22 15:27:34 +0300

    Make level 2

---
+++ DevIncubator_Autopark/Color.cs
@@ -0,0 +1,13 @@
+﻿namespace DevIncubator_Autopark
+{
+	public enum Color
+	{
+		Red,
+		Blue,
+		Black,
+		White,
+		Gray,
+		Grean,
+		Yellow
+	}
+}
---
+++ DevIncubator_Autopark/Vehicle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DevIncubator_Autopark
+{
+	public class Vehicle : IComparable<Vehicle>
+	{
+		public VehicleType VehicleType { get; set; }
+		public string Model { get; }
+		public string LicensePlate { get; set; }
+		public float Weight { get; set; }
+		public int YearIssue { get; set; }
+		public float Mileage { get; set; }
+		public Color CarColor { get; set; }
+		public float TankCapacity { get; set; }
+
+		public Vehicle() { }
+		public Vehicle(VehicleType vehicleType, string model, string licensePlate, float weight, int yearIssue, float mileage, Color carColor, float tankCapacity)
+		{
+			VehicleType = vehicleType;
+			Model = model;
+			LicensePlate = licensePlate;
+			Weight = weight;
+			YearIssue = yearIssue;
+			Mileage = mileage;
+			CarColor = carColor;
+			TankCapacity = tankCapacity;
+		}
+
+		public int CompareTo(Vehicle other)
+		{
+			double thisTaxCoefficient = VehicleType.TaxCoefficient;
+			double otherTaxCoefficient = other.VehicleType.TaxCoefficient;
+
+			if (thisTaxCoefficient < otherTaxCoefficient)
+				return -1;
+			else
+				return thisTaxCoefficient == otherTaxCoefficient ? 0 : 1;
+		}
+
+		public double GetCalcTaxPerMonth() => (Weight * 0.0013) + (VehicleType.TaxCoefficient * 30) + 5;
+
+		public override string ToString() => $"{VehicleType}, {Model}, {LicensePlate}, {Weight}, {YearIssue}, {Mileage}, {CarColor}, {TankCapacity}, {this.GetCalcTaxPerMonth()}";
+	}
+}
