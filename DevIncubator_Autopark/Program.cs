c1226f95681af4291357c7a021507e457bee4d3c
DenisCo1100100 <kodek_den2001@mail.ru>
2021-10-26 16:37:14 +0300

    Add changes to Vehicle

--- DevIncubator_Autopark/Program.cs
+++ DevIncubator_Autopark/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevIncubator_Autopark.Engines;
+using System;
 
 namespace DevIncubator_Autopark
 {
@@ -16,13 +17,13 @@ namespace DevIncubator_Autopark
 
 			Vehicle[] vehicles = new Vehicle[]
 			{
-				new Vehicle(vehicleTypes[0], "Volkswagen Crafter", "5427 AX-7", 2022f, 2015, 376000, ColorType.Blue, 200f),
-				new Vehicle(vehicleTypes[0], "Volkswagen Crafter", "6427 AA-7", 2500f, 2014, 227010, ColorType.White, 200f),
-				new Vehicle(vehicleTypes[0], "Electric Bus E321", "6785 BA-7", 12080f, 2019, 20451, ColorType.Grean, 200f),
-				new Vehicle(vehicleTypes[1], "Golf 5", "8682 AX-7", 1200f, 2006, 230451, ColorType.Gray, 90f),
-				new Vehicle(vehicleTypes[1], "Tesla Model S 70D", "E001 AA-7", 2200f, 2019, 10454, ColorType.White, 7000f),
-				new Vehicle(vehicleTypes[2], "Hamm HD 12VV", null, 1200f, 2016, 122, ColorType.Yellow, 90f),
-				new Vehicle(vehicleTypes[3], "МТЗ Беларус-1025.4", "1145 AB-7", 1200f, 2020, 109, ColorType.Red, 440f)
+				new Vehicle(vehicleTypes[0], new GasolineEngine(2, 8.1), "Volkswagen Crafter", "5427 AX-7", 2022f, 2015, 376000, ColorType.Blue, 200f),
+				new Vehicle(vehicleTypes[0], new GasolineEngine(2.18, 8.5), "Volkswagen Crafter", "6427 AA-7", 2500f, 2014, 227010, ColorType.White, 200f),
+				new Vehicle(vehicleTypes[0], new ElectricalEngine(50f), "Electric Bus E321", "6785 BA-7", 12080f, 2019, 20451, ColorType.Grean, 200f),
+				new Vehicle(vehicleTypes[1], new DieselEngine(1.6, 7.2), "Golf 5", "8682 AX-7", 1200f, 2006, 230451, ColorType.Gray, 90f),
+				new Vehicle(vehicleTypes[1], new ElectricalEngine(25f), "Tesla Model S 70D", "E001 AA-7", 2200f, 2019, 10454, ColorType.White, 7000f),
+				new Vehicle(vehicleTypes[2], new DieselEngine(3.2, 25), "Hamm HD 12VV", null, 1200f, 2016, 122, ColorType.Yellow, 90f),
+				new Vehicle(vehicleTypes[3], new DieselEngine(4.75, 20.1), "МТЗ Беларус-1025.4", "1145 AB-7", 1200f, 2020, 109, ColorType.Red, 440f)
 			};
 
 			foreach (var vehicle in vehicles)
@@ -40,6 +41,19 @@ namespace DevIncubator_Autopark
 
 			Console.WriteLine($"Max milage => {VehicleHelper.GetMaxVehicleByMileage(vehicles)} " +
 							 $"\nMin milage => {VehicleHelper.GetMinVehicleByMileage(vehicles)}");
+
+			PrintEqualsVehicles(vehicles);
+		}
+
+		public static void PrintEqualsVehicles(Vehicle[] vehicles)
+		{
+			Console.WriteLine("\nEquals vehicles:");
+
+			for (int i = 1; i < vehicles.Length; i++)
+			{
+				if (vehicles[i].Equals(vehicles[i - 1]))
+					Console.WriteLine($"{vehicles[i]}\n{vehicles[i - 1]}");
+			}
 		}
 	}
 }
